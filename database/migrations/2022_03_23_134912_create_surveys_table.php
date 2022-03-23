918382ba4fe5d9fbc3befd3c4843877bdebe9f02
lionatic <ahmadhakim404@gmail.com>
2022-03-23 14:05:20 +0700

    define migration column for survey

--- database/migrations/2022_03_23_134912_create_surveys_table.php
+++ database/migrations/2022_03_23_134912_create_surveys_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,12 @@ return new class extends Migration
     {
         Schema::create('surveys', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class, 'user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug');
+            $table->tinyInteger('status');
+            $table->text('description')->nullable();
+            $table->timestamp('expired_at');
             $table->timestamps();
         });
     }
--- database/migrations/2022_03_23_135000_create_survey_questions_table.php
+++ database/migrations/2022_03_23_135000_create_survey_questions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Survey;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,11 @@ return new class extends Migration
     {
         Schema::create('survey_questions', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Survey::class, 'survey_id')->constrained()->onDelete('cascade');
+            $table->string('type');
+            $table->string('question');
+            $table->longText('description')->nullable();
+            $table->longText('data')->nullable();
             $table->timestamps();
         });
     }
